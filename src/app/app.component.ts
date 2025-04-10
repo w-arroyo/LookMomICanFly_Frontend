5172205933dd37aaeb22494ee5b991ad9705d879
Arroyo <wdonarroyo@gmail.com>
2025-04-10 20:56:35 +0200

    home page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/main_components/header/header.component';
-import { HomeCarouselComponent } from './components/secondary_components/home-carousel/home-carousel.component';
 import { FooterComponent } from './components/secondary_components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, HomeCarouselComponent, FooterComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeCarouselComponent } from './components/secondary_components/home-carousel/home-carousel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', redirectTo:'home', pathMatch:'full'
+    },
+    {
+        path: 'home',
+        component: HomeCarouselComponent
+    },
+    {
+        path:'**', redirectTo:'home'
+    }
+
+];
