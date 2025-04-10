0c8dec710138cc0322ce6d0c18ff7c32598d8f90
Arroyo <wdonarroyo@gmail.com>
2025-04-10 20:29:33 +0200

    home page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/main_components/header/header.component';
+import { HomeCarouselComponent } from './components/secondary_components/home-carousel/home-carousel.component';
+import { FooterComponent } from './components/secondary_components/footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent],
+    imports: [RouterOutlet, HeaderComponent, HomeCarouselComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
