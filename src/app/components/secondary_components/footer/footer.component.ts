d1b6cdcc1d8585afbe47afd89f6fa76a39ac4d7b
Arroyo <wdonarroyo@gmail.com>
2025-04-28 20:50:32 +0200

    frontend v1 completed

--- src/app/components/secondary_components/footer/footer.component.ts
+++ src/app/components/secondary_components/footer/footer.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
+import { NewsletterService } from '../../../services/newsletter/newsletter.service';
+import { Subject, takeUntil } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy{
+
+  private newsletterService:NewsletterService;
+  email:string='';
+  private destroyStream:Subject<void>=new Subject<void>();
+
+  constructor(newsletterService:NewsletterService){
+    this.newsletterService=newsletterService;
+  }
+
+  send(){
+    if(this.email.trim()!==''){
+      this.newsletterService.send(this.email).pipe(
+        takeUntil(
+          this.destroyStream
+        )
+      ).subscribe({
+        next:(data)=>{
+          this.email='';
+        },
+        error:(error)=>{
+          console.log(error.error?.error);
+          this.email='';
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroyStream.next();
+    this.destroyStream.complete();
+  }
 
 }
